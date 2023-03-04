0b7b20b19e00ed18ad60a3ecb780e5c136b3155d
xiaozhao1111 <zhaoxiao20071106@gmail.com>
2023-03-04 16:20:04 +0000

    change syntax for import/export  modules and js files

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-import Manager from "./lib/Manager";
-import Engineer from "./lib/Engineer";
-import Intern from "./lib/Intern";
+import Employee from "./lib/Employee.js";
+import Manager from "./lib/Manager.js";
+import Engineer from "./lib/Engineer.js";
+import Intern from "./lib/Intern.js";
 import inquirer from 'inquirer';
 import * as path from 'path';
 import * as fs from 'fs'
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
+
+const OUTPUT_DIR = path.resolve('./', "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
+import render from "./src/page-template.js";
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+inquirer
+.prompt([
+    {
+        type: "input",
+        message:"What's the name of the team manager?",
+        name: 'managerName',
+        validate: (input) => {
+            if(input.trim()) {
+                return true;
+            } else {
+                return 'Please input a valid name!'
+            }
+        }
+
+    }
+])
