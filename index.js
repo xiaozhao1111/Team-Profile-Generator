3cc9e6505582de5fe34c700d7f3f7c26769db41c
xiaozhao1111 <zhaoxiao20071106@gmail.com>
2023-03-05 19:23:38 +0000

    inquirer input validation functions in index.js

--- index.js
+++ index.js
@@ -15,19 +15,71 @@ import render from "./src/page-template.js";
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-inquirer
-.prompt([
+/* ------------functions to valiate inquirer input---------------- */
+const validateName = (name) => {
+    const letterAndSpace = /^[A-Za-z\s]*$/;
+    // return true if name is not empty and contains letters only
+   return name.trim().match(letterAndSpace) ? true : 'Please input a valid name!';
+}
+const validateId = (id) => {
+    return Number.isInteger(id) ? true : 'Please enter a valid ID number!';
+}
+
+const validateEmail = (email) => {
+    //Ref: https://www.simplilearn.com/tutorials/javascript-tutorial/email-validation-in-javascript
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return email.match(emailRegex) ? true : 'Please input a valid name!'
+}
+
+// Ref: https://gist.github.com/aslamdoctor/6620085
+const valiateGithub = (userName) => {
+    let illegalChars = /\W/; // allow letters, numbers, and underscores
+    if (userName === '') {
+        return ('Please enter a valid username!');
+    } else if (userName.length < 5 || userName.length > 15) {
+        return ('Username must have 5-15 characters!');
+    } else if (illegalChars.test(userName)) {
+        return ('Please enter valid Username. Use only numbers and alphabets!')
+    } else {
+        return true;
+    }
+}
+/*---------------------------------------------------------------------------------------*/
+
+/*----------------------Functions to get employees' info and create objects------------------------*/
+
+const createManager = () => {
+    inquirer
+    .prompt([
     {
-        type: "input",
+        type: 'input',
         message:"What's the name of the team manager?",
         name: 'managerName',
-        validate: (input) => {
-            if(input.trim()) {
-                return true;
-            } else {
-                return 'Please input a valid name!'
-            }
-        }
-
+        validate: validateName
+    },
+    {
+        type: 'input',
+        message: `What's the id of the manager?`,
+        name: 'managerID',
+        validate: validateId
+    },
+    {
+        type: 'input',
+        message:"What's the email address of the team manager?",
+        name: 'managerEmail',
+        validate: validateEmail
+    },
+    {
+        
     }
+    
 ])
+    .then((response) =>{
+        console.log(response)
+        }
+    )
+}
+
+
+
+
